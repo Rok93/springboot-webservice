9c7be901d48768af96d2802dcf174591a1dc84d4
김경록 <goodboy3421@gmail.com>
2019-12-17 20:31:03 +0900

    [UPDATE] 모든 Posts 조회하는 기능
    findAllDesc 메소드 추가

--- src/main/java/com/rok93/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/rok93/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.rok93.book.springboot.service.posts;
 
 import com.rok93.book.springboot.domain.posts.Posts;
 import com.rok93.book.springboot.domain.posts.PostsRepository;
+import com.rok93.book.springboot.web.dto.PostsListResponseDto;
 import com.rok93.book.springboot.web.dto.PostsResponseDto;
 import com.rok93.book.springboot.web.dto.PostsSaveRequestDto;
 import com.rok93.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -37,4 +41,10 @@ public class PostsService {
         return new PostsResponseDto(entity);
     }
 
+    @Transactional(readOnly = true) // 트랜잭션 범위는 유지하되, 조회 기능만 남겨두어 조회 속도가 개선됌 (CUD 기능이 전혀 없는 서비스 메소드에서 사용하는 것을 추천)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findeAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
 }

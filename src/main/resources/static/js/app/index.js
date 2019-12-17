a9e50f30d19df4c2ca0066d4477e13031d02b3bd
김경록 <goodboy3421@gmail.com>
2019-12-17 22:15:23 +0900

    [UPDATE] 게시글 update 기능

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,11 +1,16 @@
 var index = {
-    init : function () {
+    init: function () {
         var _this = this;
-        $('#btn-save').on('click', function() {
+
+        $('#btn-save').on('click', function () {
             _this.save();
         });
+
+        $('#btn-update').on('click', function () { // 1. 'btn-update' id를 가진 HTML 엘리먼트에 click 이벤트가 발생할 때 update function을 실행하도록 이벤트 등록
+            _this.update();
+        })
     },
-    save : function () {
+    save: function () {
         var data = {
             title: $('#title').val(),
             author: $('#author').val(),
@@ -13,7 +18,7 @@ var index = {
         };
 
         $.ajax({
-            type: 'POST' ,
+            type: 'POST',
             url: '/api/v1/posts',
             dataType: 'json',
             contentType: 'application/json; charset=utf-8',
@@ -24,6 +29,28 @@ var index = {
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
+    },
+    update: function () { // 2. 신규로 추가될 update function 이다.
+        var data = {
+            title: $('#title').val(),
+            content: $('#content').val()
+        };
+
+        var id = $('#id').val()
+
+        $.ajax({
+            type: 'PUT', // 3. 여러 HTTP Method 중 PUT 메소드를 선택한다. (Controller에 있는 Api에서 이미 @PutMapping으로 선언했기 때문에 PUT을 사용해야 한다)
+            url: '/api/v1/posts/' + id, // 4. 어느 게시글을 수정할지 URL Path로 구분하기 위해 Path에 id를 추가한다.
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function () {
+            alert('글이 수정되었습니다.')
+            window.location.href = '/';
+        }).fail(function () {
+            alert(JSON.stringify(data))
+        });
+
     }
 
 };

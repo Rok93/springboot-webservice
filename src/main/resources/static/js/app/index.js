2348a116af24d7d71c28a1e66ee97e76f99d4ec4
김경록 <goodboy3421@gmail.com>
2019-12-17 22:55:21 +0900

    [UPDATE] delete 기능

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -8,6 +8,10 @@ var index = {
 
         $('#btn-update').on('click', function () { // 1. 'btn-update' id를 가진 HTML 엘리먼트에 click 이벤트가 발생할 때 update function을 실행하도록 이벤트 등록
             _this.update();
+        });
+
+        $('#btn-delete').on('click', function () {
+            _this.delete();
         })
     },
     save: function () {
@@ -51,8 +55,23 @@ var index = {
             alert(JSON.stringify(data))
         });
 
-    }
+    },
+    delete : function () {
+        var id = $('#id').val();
 
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/posts/'+id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8'
+        }).done(function () {
+            alert('글이 삭제되었습니다.')
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        })
+
+    }
 };
 
 index.init();

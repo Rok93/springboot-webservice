24e0bf8c1f5107c25890b9ab68bf105293d028a1
김경록 <goodboy3421@gmail.com>
2019-12-21 13:33:26 +0900

    gitignore 적용 오류로 캐시 삭제 후 재설정

---
+++ src/main/java/com/rok93/book/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,21 @@
+package com.rok93.book.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+/**
+ * 클래스명: PostsRepository
+ * Posts 클래스로 Database를 접근하게 해줄 인터페이스
+ * 단순히 인터페이스를 생성 후, JpaRepository<Entity 클래스, PK 타입>을 상속하면 기본적인
+ * CRUD 메소드가 자동으로 생성된다.
+ * (주의: Entity 클래스와 기본 Entity Repository는 함께 위치해야한다.
+ * Entity 클래스는 기본 Repository 없이는 제대로 역할을 할 수가 없다.)
+ */
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    // SpringDataJpa에서 제공하는 기본 메소드만으로 해결할 수 있다. 하지만 @Query가 훨씬 가독성이 좋으니 선택해서 사용하라
+    @Query("SELECT p from Posts p ORDER BY p.id DESC")
+    List<Posts> findeAllDesc();
+}
---
+++ src/main/resources/static/js/app/index.js
@@ -0,0 +1,77 @@
+var index = {
+    init: function () {
+        var _this = this;
+
+        $('#btn-save').on('click', function () {
+            _this.save();
+        });
+
+        $('#btn-update').on('click', function () { // 1. 'btn-update' id를 가진 HTML 엘리먼트에 click 이벤트가 발생할 때 update function을 실행하도록 이벤트 등록
+            _this.update();
+        });
+
+        $('#btn-delete').on('click', function () {
+            _this.delete();
+        })
+    },
+    save: function () {
+        var data = {
+            title: $('#title').val(),
+            author: $('#author').val(),
+            content: $('#content').val()
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: '/api/v1/posts',
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function () {
+            alert('글이 등록되었습니다.');
+            window.location.href = '/';  //글 등록이 성공하면 메인페이지(/)로 이동합니다.
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
+    update: function () { // 2. 신규로 추가될 update function 이다.
+        var data = {
+            title: $('#title').val(),
+            content: $('#content').val()
+        };
+
+        var id = $('#id').val()
+
+        $.ajax({
+            type: 'PUT', // 3. 여러 HTTP Method 중 PUT 메소드를 선택한다. (Controller에 있는 Api에서 이미 @PutMapping으로 선언했기 때문에 PUT을 사용해야 한다)
+            url: '/api/v1/posts/' + id, // 4. 어느 게시글을 수정할지 URL Path로 구분하기 위해 Path에 id를 추가한다.
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function () {
+            alert('글이 수정되었습니다.')
+            window.location.href = '/';
+        }).fail(function () {
+            alert(JSON.stringify(data))
+        });
+
+    },
+    delete : function () {
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/posts/'+id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8'
+        }).done(function () {
+            alert('글이 삭제되었습니다.')
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        })
+
+    }
+};
+
+index.init();
